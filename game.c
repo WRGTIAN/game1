a9bba79145c6d1138baff7796c540b9a2ebfac8b
WRGTIAN <3320875217@qq.com>
2016-11-30 21:45:29 +0800

    zhuhanshu

---
+++ game.c
@@ -0,0 +1,32 @@
+#include"ThreeChess.h"
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void FunTest()
+{
+	int select = 0;
+
+	while(1)
+	{
+		printf("------------------------------------\n");
+		printf("------------- 1. play---------------\n");
+		printf("------------- 2. exit---------------\n");
+		printf("请选择:>\n");
+		scanf("%d",&select);
+		switch(select)
+		{
+		case 1:
+			Game();
+			break;
+		case 2:
+			return;
+		default:
+			break;
+		}
+	}
+}
+
+int main()
+{
+	FunTest();
+	return 0;
+}
